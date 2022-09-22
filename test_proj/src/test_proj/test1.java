41bbe94e7bea5253b02dfaaf298c21d8d85c9fc2
nvijay484 <nvijay484@gmail.com>
2022-09-22 17:40:47 +0530

    Add files via upload

---
+++ test_proj/src/test_proj/test1.java
@@ -0,0 +1,23 @@
+package test_proj;
+
+
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String a = "vijay";
+		String b = "";
+		char ch;
+		System.out.println(a);
+		
+	
+      for (int i=0; i<a.length(); i++) {
+    	  ch = a.charAt(i);
+    	  b = ch + b;
+      }
+      
+	}
+
+}
